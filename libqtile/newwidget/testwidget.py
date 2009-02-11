59ab388b49e4cb1955c31080bbf28055fa8b7720
Ben Duffield <jebavarde@gmail.com>
2009-02-11 20:21:38 +0000

    Add newbar and newwidets - these widgets are drawn with PIL so they don't have to worry about X calls. This will also allow vertical bars to be drawn eventually

---
+++ libqtile/newwidget/testwidget.py
@@ -0,0 +1,12 @@
+from base import Widget
+
+class TestWidget(Widget):
+    def __init__(self, name, width, color, align=Widget.ALIGN_LEFT):
+        print "initialising testwidget"
+        Widget.__init__(self, name, width, align)
+        self.color = color
+
+    def draw(self, canvas):
+        w, h = canvas.size
+        canvas.paste(self.color, (0,0,w,h))
+        return canvas
---
+++ libqtile/newwidget/textbox.py
@@ -0,0 +1,34 @@
+from base import Widget
+import Image
+import ImageDraw
+import ImageFont
+
+class TextBox(Widget):
+    WIDTH_AUTO = -1
+    def __init__(self, name, text, width=WIDTH_AUTO, align=Widget.ALIGN_LEFT):
+        Widget.__init__(self, name, width, align)
+        self.text = text
+        self.font = None
+        self.textheight = 0
+
+    def _configure(self, bar, theme):
+        Widget._configure(self, bar, theme)
+        self.font = ImageFont.truetype(theme["%s_ttffont" % self.name],
+                                       theme["%s_ttffontsize" % self.name],
+                                       )
+        if self.width_req == self.WIDTH_AUTO:
+            self.width_req, self.textheight = self.font.getsize(self.text)
+        else:
+            self.textheight = self.font.getsize(self.text)[1]
+            
+            
+
+    def draw(self, canvas):
+        draw = ImageDraw.Draw(canvas)
+        y = (canvas.size[1] - self.textheight)/2
+        draw.text((0,y),
+                  self.text,
+                  font=self.font,
+                  fill=self.theme["%s_fg_normal" % self.name],
+                  )
+        return canvas
