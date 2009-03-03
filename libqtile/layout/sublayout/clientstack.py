ac0498ce0917e27189a0998e0fb1cdd929512761
Ben Duffield <jebavarde@gmail.com>
2009-03-03 19:32:41 +0000

    Add a stacking parameter to nextpos, so that windows can be ordered properly

--- libqtile/layout/sublayout/clientstack.py
+++ libqtile/layout/sublayout/clientstack.py
@@ -1,6 +1,6 @@
 from ..base import Layout
 from sublayout import SubLayout, Rect, TopLevelSubLayout
-from ... import command, utils
+from ... import command, utils, window
 from ...manager import Hooks
 
 class ClientStack(Layout):
