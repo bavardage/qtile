dca03f07d248307b8c5448287093686a91caad2f
Ben Duffield <jebavarde@gmail.com>
2009-03-15 01:16:05 +0000

    A few bugfixes to ResizableStack

--- libqtile/layout/sublayout/sublayout.py
+++ libqtile/layout/sublayout/sublayout.py
@@ -254,7 +254,7 @@ class ResizableStack(SubLayout):
     def layout(self, rect, clients):
         while len(self.client_ratios) > len(clients):
             removing = self.client_ratios[-1]
-            share = removing/len(self.client_ratios)
+            share = removing/(len(self.client_ratios)-1)
             self.client_ratios = [r+share for r in self.client_ratios[:-1]]
         while len(self.client_ratios) < len(clients):
             l = len(self.client_ratios)
@@ -290,6 +290,10 @@ class ResizableStack(SubLayout):
             index = self.windows.index(focused)
         except ValueError:
             print "no focused window in the stack"
+            return
+
+        if len(self.windows) == 1:
+            self.client_ratio[0] == 1.0
             return
 
         if index == len(self.windows) - 1: #last?
