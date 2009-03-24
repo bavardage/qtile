5da30ce28d084b308799a72d432b22de527fa78d
Ben Duffield <jebavarde@gmail.com>
2009-03-24 21:24:15 +0000

    Add to the sublayout tests.  - start tests for TopLevelSubLayout.

--- test/test_sublayout.py
+++ test/test_sublayout.py
@@ -1,5 +1,7 @@
 import libpry
-from libqtile.layout.sublayout.sublayout import Rect
+from libqtile.layout.sublayout.sublayout import Rect, VerticalStack
+from libqtile import layout
+import utils
 
 class uRect(libpry.AutoTree):
     def test_horizontal_split(self):
@@ -38,8 +40,84 @@ class uRect(libpry.AutoTree):
         r = Rect(10, 20, 30, 40)
         assert r.center == (25, 40)
 
+class VertStack(VerticalStack):
+    '''
+    Just fill in the methods ness to use this sublayout
+    '''
+    def filter(self, c):
+        return True
+    def request_rectangle(self, r, windows):
+        return (r, Rect(0,0,0,0))
+
+class Config:
+    groups = ["a", "b", "c"]
+    layouts = [
+        layout.ClientStack(
+            SubLayouts=[
+                (VertStack, {}),
+                ],
+            add_mode=layout.ClientStack.ADD_TO_BOTTOM,
+            ),
+           
+        ]
+    keys = []
+    screens = []
+    theme = None
+
+class uTopLevelSublayout(utils.QtileTests):
+    config = Config()
+    
+    def test_basic_command_interface(self):
+        tlsl = self.c.layout.sublayouts['current']
+        info = tlsl.info()
+        assert info['windows'] == []
+        assert info['name'] == 'TopLevelSubLayout'
+        # we cannot compare the sublayouts directly, since
+        # the list of sublayouts come from sublayout_names.keys() - 
+        # we cannot be sure of the ordering
+        sls = info['sublayouts']
+        for sl in ['minimised', 'maximised', 'main', 'specialtypes', 'floating']:
+            assert sl in sls
+        assert len(sls) == 5
+
+        main = tlsl.sl['main']
+        info = main.info()
+        assert info['name'] == 'VertStack'
+        assert info['windows'] == []
+        assert info['sublayouts'] == []
+
+    def test_window_filtering(self):
+        self.testWindow("one")
+        self.testWindow("two")
+        self.testWindow("three")
+        
+        tlsl = self.c.layout.sublayouts['current']
+        info = tlsl.info()
+        assert info['windows'] == ["one", "two", "three"]
+
+        assert tlsl.sl['main'].info()['windows'] == ["one", "two", "three"]
+        
+        self.c.window.toggle_floating()
+
+        assert tlsl.sl['main'].info()['windows'] == ["one", "two"]
+        assert tlsl.sl['floating'].info()['windows'] == ["three"]
+
+        self.c.window.minimise()
+
+        assert tlsl.sl['main'].info()['windows'] == ["one", "two"]
+        assert tlsl.sl['minimised'].info()['windows'] == ["three"]
+
+        self.c.window.maximise()
+
+        assert tlsl.sl['main'].info()['windows'] == ["one", "two"]
+        assert tlsl.sl['maximised'].info()['windows'] == ["three"]
+
+        #TODO: Test special types
 
 
 tests = [
     uRect(),
+    utils.xfactory(xinerama=False), [
+        uTopLevelSublayout(),
+        ],
     ]
